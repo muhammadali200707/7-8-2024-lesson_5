00e7674f494d8296ac8df5f1b8416e57d37f0efe
Shohnur <Shohnur@gamil.com>
2024-07-11 19:04:03 +0500

    changes files

--- book/views.py
+++ book/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from book.models import Book
+from django.contrib.auth.decorators import login_required
 
 
 def home(request):
     return render(request, 'home.html')
 
 
+@login_required()
 def books(request):
     if request.method == 'POST':
         search = request.POST['search']
@@ -19,13 +21,10 @@ def books(request):
     return render(request, 'books.html', {'books': books})
 
 
+@login_required()
 def book_detail(request, id):
     book = Book.objects.get(id=id)
     if book:
         return render(request, 'book_detail.html', {'book': book, 'message': "Successfully"})
     else:
         return render(request, 'book_detail.html', {"message": "We couldn't find such as book"})
-
-
-
-
--- core/settings.py
+++ core/settings.py
@@ -1,11 +1,11 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -17,8 +17,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+LOGIN_URL = 'login'
 # Application definition
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -62,7 +63,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -76,7 +76,6 @@ DATABASES = {
         "PORT": os.getenv("PORT"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -96,7 +95,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -107,7 +105,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
